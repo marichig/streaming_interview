7ff3a9abeb08334ae6411bbde45df032e7d3eefe
marichig <31973288+marichig@users.noreply.github.com>
2025-08-03 15:16:01 -0400

    Initial logic flow; constants

---
+++ interview/constants.py
@@ -0,0 +1,18 @@
+# json fields
+TYPE = "type"
+STATION_NAME = "stationName"
+TIMESTAMP = "timestamp"
+TEMPERATURE = "temperature"
+COMMAND = "command"
+AS_OF = "asOf"
+STATIONS = "stations"
+HIGH = "high"
+LOW = "low"
+
+#message types
+SAMPLE = "sample"
+CONTROL = "control"
+
+#command types
+SNAPSHOT = "snapshot"
+RESET = "reset"
--- interview/weather.py
+++ interview/weather.py
@@ -1,6 +1,47 @@
 from typing import Any, Iterable, Generator
+import json
+from .constants import (
+    TYPE,
+    STATION_NAME,
+    TIMESTAMP,
+    TEMPERATURE,
+    COMMAND,
+    AS_OF,
+    STATIONS,
+    HIGH,
+    LOW,
+    SAMPLE,
+    CONTROL,
+    SNAPSHOT,
+    RESET,
+)
 
+
+latest_timestamp = -1
+stations = {}
 
 def process_events(events: Iterable[dict[str, Any]]) -> Generator[dict[str, Any], None, None]:
     for line in events:
-        yield line
+        if line[TYPE] == SAMPLE:
+            latest_timestamp = line[TIMESTAMP]
+            process_sample(line)
+        if line[TYPE] == CONTROL:
+            yield process_control(line)
+        else:
+            raise Exception #fill in later
+        
+def process_sample(sample: dict[str, Any]):
+    station = sample[STATION_NAME]
+    temperature = sample[TEMPERATURE]
+
+    if station in stations:
+        if temperature > stations[stations][HIGH]:
+            stations[station][HIGH] = temperature
+        elif temperature < stations[station[LOW]]:
+            stations[station[LOW]] = temperature
+    else:
+        stations[station] = {HIGH: temperature, LOW: temperature}
+
+
+def process_control(message: dict[str, Any]) -> dict[str,Any]:
+    return {}
